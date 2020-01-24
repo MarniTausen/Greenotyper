2663e1507ad610e0afab65734c73862a722b972f
Marni Tausen <marni16ox@gmail.com>
2020-01-24 11:23:08 +0100

    v0.6.0 version released

--- test_sample.py
+++ test_sample.py
@@ -481,8 +481,6 @@ class TestsPipelinePlanner(unittest.TestCase):
         self.mainwindow.GUI.setDefaultValues()
 
         self.mainwindow.GUI.FindPlants(multithread=False)
-        while self.mainwindow.GUI.network_is_running:
-            continue
         self.mainwindow.GUI.ImageMask(multithread=False)
 
         self.mainwindow.GUI.TestCrop()
